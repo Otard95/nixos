33c2612a5c6d8d1a2b6fddebb575aa1c94e389e1
Stian M <otard.code@proton.me>
2024-11-10 18:36:13 +0100

    chore(home): Remove last remnants of old neovim config

--- home/home.nix
+++ home/home.nix
@@ -59,13 +59,8 @@ in
     "gtk-4.0/gtk.css".source = "${gtk4Dir}/gtk.css";
     "gtk-4.0/gtk-dark.css".source = "${gtk4Dir}/gtk-dark.css";
 
-    # nvim.source = mkOutOfStoreSymlink "/etc/nixos/.config/nvim";
   };
 
   home.packages = with pkgs; [
   ];
-
-  programs = {
-    # neovim = (import ./nvim.nix { inherit pkgs; });
-  };
 }
