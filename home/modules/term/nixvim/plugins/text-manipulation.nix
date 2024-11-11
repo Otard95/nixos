0c925cf23b076eae9765bf7f5542fdff89f49d3b
Stian M <otard.code@proton.me>
2024-11-11 16:14:38 +0100

    fix(home): Properly setup multicursors for nixvim

--- home/modules/term/nixvim/plugins/text-manipulation.nix
+++ home/modules/term/nixvim/plugins/text-manipulation.nix
@@ -1,5 +1,20 @@
 { ... }:
 {
+  programs.nixvim.keymaps = [
+    { # Start multicursor from normal mode
+      mode = "n";
+      key = "<leader>m";
+      action = "<cmd>MCstart<cr>";
+      options = { silent = true; };
+    }
+    { # Start multicursor from visual mode
+      mode = "v";
+      key = "<leader>m";
+      action = "<cmd>MCstart<cr>";
+      options = { silent = true; };
+    }
+  ];
+
   programs.nixvim.plugins = {
     commentary.enable = true;
     vim-surround.enable = true;
