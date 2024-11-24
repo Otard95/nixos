f55b26a252a19ba236e94841e37e834eaa82fe15
Stian M <otard.code@proton.me>
2024-11-24 13:29:08 +0100

    feat(power-menu): Add splash image for power menu

--- home-manager-modules/power-menu/rofi.nix
+++ home-manager-modules/power-menu/rofi.nix
@@ -20,6 +20,8 @@ in {
         themeTempl;
     in {
       "rofi/power-menu/theme.rasi".text = themeFile;
+      "rofi/power-menu/splash.jpg".source =
+        ./splash-images/lynnette_space_suit_20240205_04_by_kai_artworks_dgu3229-pre.jpg;
       "power-menu.sh".source = ./start.sh;
     };
   };
