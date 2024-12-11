6fb02a9ee37975a9cc24892978fc839b9c321ed5
Stian M <otard.code@proton.me>
2024-12-11 17:18:56 +0100

    feat: Support for logitech periferals wip

--- hosts/terra/configuration.nix
+++ hosts/terra/configuration.nix
@@ -47,6 +47,12 @@
     };
   };
 
+  # TODO: move to module
+  hardware.logitech.wireless = {
+    enable = true;
+    enableGraphical = true;
+  };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   # environment.systemPackages = with pkgs; [ ];
