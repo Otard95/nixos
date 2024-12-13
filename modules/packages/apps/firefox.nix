e15eee803c4e01002e627ad0e2d97c20669e45af
Stian M <otard.code@proton.me>
2024-12-13 16:44:43 +0100

    fix(firefox): Enable smooth scroll

--- modules/packages/apps/firefox.nix
+++ modules/packages/apps/firefox.nix
@@ -6,6 +6,9 @@ in {
   options.modules.packages.apps.firefox.enable = lib.mkEnableOption "firefox";
 
   config = lib.mkIf enable {
-    environment.systemPackages = with pkgs; [ firefox ];
+    environment = {
+      systemPackages = with pkgs; [ firefox ];
+      sessionVariables.MOZ_USE_XINPUT2 = "1";
+    };
   };
 }
