55bfa0e1b4a3c647c7211920c8fb54470a2de34d
Stian M <otard.code@proton.me>
2025-01-07 13:29:00 +0100

    chore(nixpkgs-stable): Update

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-24.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-24.11";
 
     # Home Manager
     home-manager = {
