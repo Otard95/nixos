ff5414da6362203225e0c20cf39eb0316cdb3af4
Stian M <otard.code@proton.me>
2025-01-08 09:57:18 +0100

    fix(waybar): Allow detecting capslock state

--- hosts/phobos/configuration.nix
+++ hosts/phobos/configuration.nix
@@ -32,7 +32,7 @@
   users.users.otard = {
     isNormalUser = true;
     description = "Stian";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "input" ];
     # packages = with pkgs; [];
   };
 
