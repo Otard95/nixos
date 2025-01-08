a165a011f97751b12efd180f91537b3e86681a28
Stian M <otard.code@proton.me>
2025-01-08 09:56:51 +0100

    fix(zen-browser): Set MOZ_USE_XINPUT2 env when installed

--- modules/packages/apps/zen-browser.nix
+++ modules/packages/apps/zen-browser.nix
@@ -6,20 +6,28 @@ in {
   options.modules.packages.apps.zen-browser.enable = lib.mkEnableOption "zen-browser";
 
   config = lib.mkIf enable {
-    environment.systemPackages = with pkgs; [
-      inputs.zen-browser.packages."${system}".default
-      (makeDesktopItem {
-        name = "zen-browser-personal";
-        desktopName = "Zen Browser";
-        genericName = "Personal";
-        exec = "zen -P Personal";
-        icon = "zen";
-        startupWMClass = "zen-alpha";
-        categories = [ "Network" "WebBrowser" ];
-        startupNotify = true;
-        terminal = false;
-        keywords = [ "Internet" "WWW" "Browser" "Web" "Explorer" ];
-      })
-    ];
+
+    environment = {
+
+      sessionVariables.MOZ_USE_XINPUT2 = "1";
+
+      systemPackages = with pkgs; [
+        inputs.zen-browser.packages."${system}".default
+        (makeDesktopItem {
+          name = "zen-browser-personal";
+          desktopName = "Zen Browser";
+          genericName = "Personal";
+          exec = "zen -P Personal";
+          icon = "zen";
+          startupWMClass = "zen-alpha";
+          categories = [ "Network" "WebBrowser" ];
+          startupNotify = true;
+          terminal = false;
+          keywords = [ "Internet" "WWW" "Browser" "Web" "Explorer" ];
+        })
+      ];
+
+    };
+
   };
 }
