7b837a8633c535ac608a124e66cd9d23e458d1a2
Stian M <otard.code@proton.me>
2025-05-19 14:09:58 +0200

    fix(wezterm): Make `is_vim` more robust

--- home-manager-modules/term/wezterm/utils/vim.lua
+++ home-manager-modules/term/wezterm/utils/vim.lua
@@ -1,21 +1,22 @@
-local wezterm = require 'wezterm'
+local wezterm = require 'wezterm' --[[@as Wezterm]]
 local str = require 'utils.string'
 
 local M = {}
 
+---@param pane Pane
+---@return boolean
 function M.is_vim(pane)
   local process_info = pane:get_foreground_process_info()
-  local process_name = ''
+
   if process_info == nil then
     local user_vars = pane:get_user_vars()
-    process_name = user_vars and user_vars.WEZTERM_PROG
     wezterm.log_info('[is_vim()] is mux pane', { pane = pane, user_vars = user_vars })
-  else
-    process_name = process_info and process_info.name
-    wezterm.log_info('[is_vim()] normal pane', { pane = pane, process_info = process_info })
+    return user_vars and user_vars.WEZTERM_PROG and str.starts_with(user_vars.WEZTERM_PROG, 'vi')
   end
 
-  return process_name == "nvim" or process_name == "vim" or (str and str.starts_with(process_name, 'vi'))
+  local process_name = process_info and process_info.name
+  wezterm.log_info('[is_vim()] normal pane', { pane = pane, process_info = process_info })
+  return process_name == "nvim" or process_name == "vim"
 end
 
 return M
