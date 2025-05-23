8c250ea179f2374f3a8f4b7ea17ba8077e3c86fb
Stian M <otard.code@proton.me>
2025-05-23 14:26:16 +0200

    tweak(nixvim): Markdown rendering

--- nixvim/plugins/markdown.nix
+++ nixvim/plugins/markdown.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 let
   cfg = config.modules.nixvim.plugins.markdown;
   enable = cfg.enable;
@@ -7,34 +7,43 @@ in {
     lib.mkEnableOption "markdown plugins";
 
   config = lib.mkIf enable {
-    programs.nixvim.plugins.render-markdown = {
-      enable = true;
+    programs.nixvim = {
+      extraPlugins = [
+        pkgs.vimPlugins.vim-markdown-toc
+      ];
 
-      settings = {
-        checkbox = {
-          unchecked = {
-            icon = "󰄱";
+      plugins.render-markdown = {
+        enable = true;
+
+        settings = {
+          bullet = {
+            left_pad = 2;
           };
-          checked = {
-            icon = "󰱒";
+          checkbox = {
+            unchecked = {
+              icon = "󰄱";
+            };
+            checked = {
+              icon = "󰱒";
+            };
+            custom = {
+              todo = { raw = "[-]"; rendered = "󰥔"; highlight = "RenderMarkdownTodo"; };
+              follow_up = { raw = "[>]"; rendered = "󰬪"; highlight = "WarningMsg"; };
+              canceled = { raw = "[~]"; rendered = "󰅙"; highlight = "ErrorMsg"; };
+            };
           };
-          custom = {
-            todo = { raw = "[-]"; rendered = "󰥔"; highlight = "RenderMarkdownTodo"; };
-            follow_up = { raw = "[>]"; rendered = "󰬪"; highlight = "WarningMsg"; };
-            canceled = { raw = "[~]"; rendered = "󰅙"; highlight = "ErrorMsg"; };
+          code = {
+            # Width of the code block background:
+            #  block: width of the code block
+            #  full:  full width of the window
+            width = "block";
+            # Amount of padding to add to the left of code blocks
+            left_pad = 2;
+            # Amount of padding to add to the right of code blocks when width is "block"
+            right_pad = 1;
+            # Minimum width to use for code blocks when width is "block"
+            min_width = 80;
           };
-        };
-        code = {
-          # Width of the code block background:
-          #  block: width of the code block
-          #  full:  full width of the window
-          width = "block";
-          # Amount of padding to add to the left of code blocks
-          left_pad = 2;
-          # Amount of padding to add to the right of code blocks when width is "block"
-          right_pad = 1;
-          # Minimum width to use for code blocks when width is "block"
-          min_width = 80;
         };
       };
     };
