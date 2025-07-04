3d0d6661915b4e77bd8fd058c2ed0057aa9725fb
Stian M <otard.code@proton.me>
2025-07-04 10:24:52 +0200

    feat(nixvim): Add icons to cmp

--- nixvim/plugins/cmp.nix
+++ nixvim/plugins/cmp.nix
@@ -9,36 +9,43 @@ in {
     lib.mkEnableOption "cmp plugin";
 
   config = lib.mkIf enable {
-    programs.nixvim.plugins.cmp = {
-      enable = true;
+    programs.nixvim.plugins = {
+      cmp = {
+        enable = true;
 
-      settings = {
-        completion.completeopt = "menu,menuone,noinsert";
-        preselect = nixvim.mkRaw "cmp.PreselectMode.None";
-        # mapping = {
-        #   "<Tab>" = "cmp.mapping.confirm({ select = true })";
-        #   "<C-Space>" = "cmp.mapping.complete()";
-        #   "<C-u>" = "cmp.mapping.scroll_docs(-4)";
-        #   "<C-d>" = "cmp.mapping.scroll_docs(4)";
-        # };
-        mapping = nixvim.mkRaw ''
-          cmp.mapping.preset.insert({
-            ['<Tab>'] = cmp.mapping.confirm({ select = true }),
-            ['<C-Space>'] = cmp.mapping.complete(),
-            ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-            ['<C-d>'] = cmp.mapping.scroll_docs(4),
-          })
-        '';
-        sources = [
-          { name = "path"; }
-          { name = "nvim_lsp"; }
-          { name = "nvim_lua"; }
-          { name = "luasnip"; }
-          { name = "buffer"; }
-        ];
-        snippet.expand = nixvim.mkRaw ''
-          function(args) require('luasnip').lsp_expand(args.body) end
-        '';
+        settings = {
+          completion.completeopt = "menu,menuone,noinsert";
+          preselect = nixvim.mkRaw "cmp.PreselectMode.None";
+          # mapping = {
+          #   "<Tab>" = "cmp.mapping.confirm({ select = true })";
+          #   "<C-Space>" = "cmp.mapping.complete()";
+          #   "<C-u>" = "cmp.mapping.scroll_docs(-4)";
+          #   "<C-d>" = "cmp.mapping.scroll_docs(4)";
+          # };
+          mapping = nixvim.mkRaw ''
+            cmp.mapping.preset.insert({
+              ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+              ['<C-Space>'] = cmp.mapping.complete(),
+              ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+              ['<C-d>'] = cmp.mapping.scroll_docs(4),
+            })
+          '';
+          sources = [
+            { name = "path"; }
+            { name = "nvim_lsp"; }
+            { name = "nvim_lua"; }
+            { name = "luasnip"; }
+            { name = "buffer"; }
+          ];
+          snippet.expand = nixvim.mkRaw ''
+            function(args) require('luasnip').lsp_expand(args.body) end
+          '';
+        };
+      };
+
+      lspkind = {
+        enable = true;
+        cmp.enable = true;
       };
     };
   };
