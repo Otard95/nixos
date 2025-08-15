c3691641419d7f1b196cc6d586fc0980b2d84a94
Stian M <otard.code@proton.me>
2025-08-15 12:29:12 +0200

    fix(pay-respects): Add rependency

--- home-manager-modules/term/pay-respects.nix
+++ home-manager-modules/term/pay-respects.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 let
   cfg = config.modules.term.pay-respects;
   enable = cfg.enable;
@@ -7,6 +7,8 @@ in {
     lib.mkEnableOption "pay-respects";
 
   config = lib.mkIf enable {
+
+    home.packages = [ pkgs.nix-index ];
 
     programs.pay-respects = {
       enable = true;
