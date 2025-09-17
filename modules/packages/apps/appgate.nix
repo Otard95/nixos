ae1b1b13461d586ddcd276b08d5bd9aea4485738
Stian M <otard.code@proton.me>
2025-09-17 07:14:49 +0200

    feat(appgate): Update

--- modules/packages/apps/appgate.nix
+++ modules/packages/apps/appgate.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 let
   cfg = config.modules.packages.apps.appgate;
   enable = cfg.enable;
@@ -7,6 +7,18 @@ in {
     lib.mkEnableOption "appgate";
 
   config = lib.mkIf enable {
+
+    nixpkgs.overlays = [
+      (final: prev: {
+        appgate-sdp = prev.appgate-sdp.overrideAttrs (old: {
+          version = "6.5.3";
+          src = pkgs.fetchurl {
+            url = "https://bin.appgate-sdp.com/${lib.versions.majorMinor "6.5.3"}/client/appgate-sdp_6.5.3_amd64.deb";
+            sha256 = "sha256-MyC28cOTO9EpvHvlNWtdNRbuywl0uBD8G5+cACBzMRY=";
+          };
+        });
+      })
+    ];
 
     programs.appgate-sdp.enable = true;
 
