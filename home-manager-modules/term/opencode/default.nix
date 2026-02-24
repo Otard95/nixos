d04a864e4f8ac17f251878ab9da0d811cb47bc59
Stian M <otard.code@proton.me>
2026-02-24 12:36:08 +0100

    feat(opencode): Update keybinds

--- home-manager-modules/term/opencode/default.nix
+++ home-manager-modules/term/opencode/default.nix
@@ -14,6 +14,25 @@ in {
       settings = {
         theme = lib.mkForce "catppuccin-frappe-transparent";
         permission = import ./permissions.nix;
+        keybinds = {
+          messages_first = "ctrl+g";
+          messages_last = "ctrl+alt+g";
+          messages_page_up = "ctrl+u,ctrl+alt+b";
+          messages_page_down = "ctrl+d,ctrl+alt+f";
+          input_submit = "ctrl+return";
+          input_newline = "return,shift+return,alt+return";
+          input_delete_to_line_start = "none";
+          input_delete_to_line_end = "none";
+          input_delete = "delete,shift+delete";
+          input_buffer_home = "pageup";
+          input_buffer_end = "pagedown";
+          input_select_buffer_home = "shift+pageup";
+          input_select_buffer_end = "shift+pagedown";
+          input_line_home = "home,ctrl+a";
+          input_line_end = "end,ctrl+e";
+          input_select_line_home = "shift+home,ctrl+shift+a";
+          input_select_line_end = "shift+end,ctrl+shift+e";
+        };
       };
 
       themes = {
