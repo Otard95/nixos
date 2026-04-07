942f796aeccb69d756e7b72b7428c6518fbc4813
Stian M <otard.code@proton.me>
2026-04-07 09:47:41 +0200

    fix(nixvim): Treesitter grammar for kulala

--- nixvim/plugins/kulala.nix
+++ nixvim/plugins/kulala.nix
@@ -8,24 +8,35 @@ in {
   options.modules.nixvim.plugins.kulala.enable =
     lib.mkEnableOption "kulala plugin";
 
-  config = lib.mkIf enable {
+  config = let
+    kulala_src = pkgs.fetchFromGitHub {
+      owner = "mistweaverco";
+      repo = "kulala.nvim";
+      rev = "5fe5c671b7c579fdc0556d4bf3e117a1c3229393";
+      hash = "sha256-BZjGh+8V1vALotrLVM2aiCESZghAvfE2/Yjqhpc75x8=";
+      fetchSubmodules = true;
+    };
+
+    treesitter-kulala-http-grammar = pkgs.tree-sitter.buildGrammar {
+      language = "kulala_http";
+      version = "5.3.1";
+      src = kulala_src;
+      location = "lua/tree-sitter";
+    };
+  in lib.mkIf enable {
 
     programs.nixvim = {
 
       extraPackages = [ pkgs.prettier ];
+
+      plugins.treesitter.grammarPackages = [ treesitter-kulala-http-grammar ];
 
       plugins.kulala = {
         enable = true;
 
         package = pkgs.vimUtils.buildVimPlugin {
           name = "kulala.nvim";
-          src = pkgs.fetchFromGitHub {
-            owner = "mistweaverco";
-            repo = "kulala.nvim";
-            rev = "5fe5c671b7c579fdc0556d4bf3e117a1c3229393";
-            hash = "sha256-BZjGh+8V1vALotrLVM2aiCESZghAvfE2/Yjqhpc75x8=";
-            fetchSubmodules = true;
-          };
+          src = kulala_src;
           nvimSkipModule = [ "cli.kulala_cli" ];
         };
 
