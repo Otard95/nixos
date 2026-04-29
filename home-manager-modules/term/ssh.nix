3a3bb86976f3e5a366c783f4212b8002c7a48cd1
Stian M <otard.code@proton.me>
2026-04-29 15:54:38 +0200

    feat(ssh): Setup for yubikey ssh keys

--- home-manager-modules/term/ssh.nix
+++ home-manager-modules/term/ssh.nix
@@ -2,9 +2,25 @@
 let
   cfg = config.modules.term.ssh;
   enable = cfg.enable;
+
+  personal_identity_files = [
+    "~/.ssh/id_ed25519_sk_rk_yubikey_personal_primary"
+    "~/.ssh/id_ed25519_sk_rk_yubikey_personal_secondary"
+  ];
+  work_identity_files = [
+    "~/.ssh/id_ed25519_sk_rk_yubikey_work_primary"
+    "~/.ssh/id_ed25519_sk_rk_yubikey_work_secondary"
+  ];
 in {
-  options.modules.term.ssh.enable =
-    lib.mkEnableOption "ssh";
+  options.modules.term.ssh = {
+    enable = lib.mkEnableOption "ssh";
+
+    identity-preset = lib.mkOption {
+      description = "Identity file preset";
+      type = lib.types.enum [ "personal" "work" ];
+      default = "personal";
+    };
+  };
 
   config = lib.mkIf enable {
 
@@ -14,17 +30,32 @@ in {
       enableDefaultConfig = false;
 
       matchBlocks = {
+        "*" = {
+          host = "*";
+          identityFile = if cfg.identity-preset == "personal"
+             then personal_identity_files
+             else work_identity_files;
+        };
         "gitea.core-lab.net" = {
           host = "gitea.core-lab.net";
           hostname = "gitea.internal";
           user = "git";
           port = 22;
+          identityFile = personal_identity_files;
         };
         jove = {
           host = "jove";
           hostname = "core-lab.net";
           user = "otard";
           port = 22;
+          identityFile = personal_identity_files;
+        };
+        luna = {
+          host = "luna";
+          hostname = "luna-ssh.internal";
+          user = "otard";
+          port = 22;
+          identityFile = personal_identity_files;
         };
       };
 
