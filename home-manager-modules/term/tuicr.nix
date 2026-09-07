0e38bb1be5ad521de1fd9b4218b1086e154b34cb
Stian M <otard.code@proton.me>
2026-09-07 08:51:19 +0200

    tweak(tuicr): Vim input comments

--- home-manager-modules/term/tuicr.nix
+++ home-manager-modules/term/tuicr.nix
@@ -2,6 +2,8 @@
 let
   cfg = config.modules.term.tuicr;
   enable = cfg.enable;
+
+  tomlFormat = pkgs.formats.toml { };
 in {
   options.modules.term.tuicr.enable =
     lib.mkEnableOption "tuicr";
@@ -9,6 +11,11 @@ in {
   config = lib.mkIf enable {
 
     home.packages = [ pkgs.tuicr ];
+ 
+    xdg.configFile."tuicr/config.toml".source = tomlFormat.generate "tuicr-config.toml" {
+      theme = "catppuccin-frappe";
+      comment_vim = true;
+    };
 
     modules.term.bash.bindToSecret.tuicr.GITHUB_TOKEN = "github/token/cli";
 
