a1e9d395868f58b5adb2f472d63b8d6699da870b
Stian M <otard.code@proton.me>
2026-09-09 08:58:54 +0200

    fix(nixvim): Tmp treesitter fix

--- nixvim/plugins/treesitter.nix
+++ nixvim/plugins/treesitter.nix
@@ -14,7 +14,33 @@ in {
           url = "https://raw.githubusercontent.com/nvim-treesitter/nvim-treesitter/master/queries/ecma/highlights.scm";
           sha256 = "sha256-N4NFR+uqnBYMrYfqvTg4fUcisbQNRLq1TY5x0f7/m54=";
         };
+        "queries/typescript/highlights.scm".source =
+          "${pkgs.vimPlugins.nvim-treesitter}/runtime/queries/typescript/highlights.scm";
+        "queries/typescript/folds.scm".source =
+          "${pkgs.vimPlugins.nvim-treesitter}/runtime/queries/typescript/folds.scm";
+        "queries/typescript/indents.scm".source =
+          "${pkgs.vimPlugins.nvim-treesitter}/runtime/queries/typescript/indents.scm";
+        "queries/typescript/injections.scm".source =
+          "${pkgs.vimPlugins.nvim-treesitter}/runtime/queries/typescript/injections.scm";
+        "queries/typescript/locals.scm".source =
+          "${pkgs.vimPlugins.nvim-treesitter}/runtime/queries/typescript/locals.scm";
+        "queries/tsx/highlights.scm".source =
+          "${pkgs.vimPlugins.nvim-treesitter}/runtime/queries/tsx/highlights.scm";
+        "queries/tsx/folds.scm".source =
+          "${pkgs.vimPlugins.nvim-treesitter}/runtime/queries/tsx/folds.scm";
+        "queries/tsx/indents.scm".source =
+          "${pkgs.vimPlugins.nvim-treesitter}/runtime/queries/tsx/indents.scm";
+        "queries/tsx/injections.scm".source =
+          "${pkgs.vimPlugins.nvim-treesitter}/runtime/queries/tsx/injections.scm";
+        "queries/tsx/locals.scm".source =
+          "${pkgs.vimPlugins.nvim-treesitter}/runtime/queries/tsx/locals.scm";
       };
+      extraConfigLua = ''
+        -- nvim-treesitter-grammars uses is-not? in js/jsx queries but the
+        -- installed nvim-treesitter plugin version doesn't register that predicate.
+        vim.treesitter.query.add_predicate('is-not?', function() return true end, { force = true })
+      '';
+
 
       plugins.treesitter = {
         enable = true;
